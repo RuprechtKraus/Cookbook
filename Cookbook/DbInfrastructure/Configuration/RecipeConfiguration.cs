a252dde34fba3af3b997c273332b5afcccf0e3e0
Ruprecht <43097128+RuprechtKraus@users.noreply.github.com>
2021-06-28 00:00:08 +0300

    Configured database

---
+++ Cookbook/DbInfrastructure/Configuration/RecipeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Cookbook.Entities.Models;
+
+namespace Cookbook.DbInfrastructure.Configuration
+{
+    public class RecipeConfiguration : IEntityTypeConfiguration<Recipe>
+    {
+        public void Configure( EntityTypeBuilder<Recipe> builder )
+        {
+            builder.ToTable( "Recipes" ).HasKey( r => r.Id );
+
+            builder.Property( r => r.Title ).IsRequired().HasMaxLength( 60 );
+            builder.Property( r => r.Desc ).IsRequired().HasMaxLength( 150 );
+            builder.Property( r => r.Likes ).HasDefaultValue( 0 );
+            builder.Property( r => r.Favs ).HasDefaultValue( 0 );
+            builder.Property( r => r.CookingTime ).IsRequired();
+            builder.Property( r => r.Servings ).IsRequired();
+
+            builder.HasOne<User>().WithMany( u => u.Recipes ).HasForeignKey(r => r.UserId);
+            builder.HasMany( r => r.Tags ).WithMany( t => t.Recipes );
+            builder.HasMany( r => r.Ingredients );
+            builder.HasMany( r => r.Steps );
+        }
+    }
+}
